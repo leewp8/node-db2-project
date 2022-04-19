74e0ebb8da720c3078d68e2f6c5a110504123c9d
William Lee <leewp8@gmail.com>
2022-04-19 17:03:48 -0400

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
 // DO YOUR MAGIC
+const express = require('express');
+const Car = require('./cars-model')
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+module.exports = router
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
     tbl.string('vin', 17).notNullable().unique();
     tbl.string('make').notNullable();
     tbl.string('model').notNullable();
-    tbl.numeric('mileage').unsigned().notNullable();
+    tbl.integer('mileage').unsigned().notNullable();
     tbl.string('title')
     tbl.string('transmission')
   })
